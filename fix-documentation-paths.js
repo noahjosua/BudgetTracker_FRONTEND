8d5a0b678063b0b827d62cd5f7ac7e9f7ecd3378
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-07-02 15:36:38 +0200

    add redirects

--- fix-documentation-paths.js
+++ fix-documentation-paths.js
@@ -5,8 +5,9 @@ const docsDir = 'dist/budget-tracker-frontend/browser/documentation';
 
 function fixPaths(filePath) {
   let content = fs.readFileSync(filePath, 'utf8');
-  content = content.replace(/href="\.\//g, 'href="/documentation/');
+  content = content.replace(/href="\.\//g, 'href="/docs/');
   content = content.replace(/src="\.\//g, 'src="/documentation/');
+  content = content.replace(/href="(?!http|\/docs)/g, 'href="/docs/');
   fs.writeFileSync(filePath, content);
 }
 
